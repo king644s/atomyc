27406015705391c364d9bf971ab94160d00791b9
king644s <sonivatsal3@gmail.com>
2024-03-15 18:55:19 +0530

    insights data udpated

--- data/data.mjs
+++ data/data.mjs
@@ -5,7 +5,7 @@ export var insightsData = [
       "In today's digital landscape, video has become a dominant force, reshaping the way people interact with brands and access information. At Atomyc, we're deeply engaged in leveraging the potential of video content, recognizing its transformative impact. Through this blog, we aim to provide valuable insights and practical guidance to help you navigate this evolving terrain. Join us as we explore the latest trends, innovative strategies, and equip you with the tools to succeed in this dynamic era of visual communication.",
     thumbnail:
       "https://res.cloudinary.com/dhbfad5qc/image/upload/v1708504654/common/leaves-6975462_1280_j3a6uf.webp",
-    page: "../insights/howweareunlockinggrowthintheageofvideodomination.html",
+    page: "./insights/howweareunlockinggrowthintheageofvideodomination.html",
   },
   {
     title: "Find Out How We Are Transforming Marketing Strategies with AI.",
@@ -13,7 +13,7 @@ export var insightsData = [
       "The fast progress of artificial intelligence (AI) has completely changed how businesses work. It's affecting nearly every industry, including marketing. AI helps marketers access loads of data and analytics they couldn't get before. This means they can create better-targeted marketing plans, which are reshaping how businesses connect with their audience. \nAt Atomyc, we stay ahead of the curve by embracing cutting-edge technologies. AI is no longer a futuristic notion; it's actively transforming how brands connect with their audiences. This blog dives into the exciting world of AI-powered marketing, exploring how it's revolutionizing strategies and propelling brands toward success.",
     thumbnail:
       "https://res.cloudinary.com/dhbfad5qc/image/upload/v1708504654/common/leaves-6975462_1280_j3a6uf.webp",
-    page: "../insights/findouthowwearetransformingmarketingstrategieswithAI.html",
+    page: "./insights/findouthowwearetransformingmarketingstrategieswithAI.html",
   },
   {
     title:
@@ -22,6 +22,6 @@ export var insightsData = [
     "In today's whirlwind of digital content, grabbing hold of your audience's attention is like finding a needle in a haystack. At Atomyc, we know firsthand the challenge of standing out amidst the noise and forging authentic connections with our audience. That's why we pour our hearts and minds into crafting campaigns that not only catch eyes but also touch hearts and minds. We believe in the power of merging creativity with data-driven strategies to produce impactful marketing campaigns for our clients. Our philosophy is straightforward yet potent: we let the data illuminate our creative path. Jyoti Venkatesh, our co-founder, often says, In the ever-evolving landscape of content, blending creativity with data-driven insights is not just an option, it's a necessity. ",
     thumbnail:
     "https://res.cloudinary.com/dhbfad5qc/image/upload/v1708504654/common/leaves-6975462_1280_j3a6uf.webp",
-    page: "../insights/here'showwecraftcompellingcampaignsdrivenbydata-ledcommunication.html",
+    page: "./insights/here'showwecraftcompellingcampaignsdrivenbydata-ledcommunication.html",
   },
 ];
