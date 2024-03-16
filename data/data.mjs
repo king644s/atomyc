92af2f64d5ffa3797b36b599909bce406d6894eb
king644s <sonivatsal3@gmail.com>
2024-03-16 19:28:49 +0530

    data and css udpated

--- data/data.mjs
+++ data/data.mjs
@@ -1,26 +1,26 @@
 export var insightsData = [
   {
-    title: "Crafting Compelling Campaigns Driven by Data-Led Communication.",
+    title: "Compelling Campaigns Driven by Data-Led Communication",
     description:
       "In today's whirlwind of digital content, grabbing hold of your audience's attention is like finding a needle in a haystack. At Atomyc, we know firsthand the challenge of standing out amidst the noise and forging authentic connections with our audience. That's why we pour our hearts and minds into crafting campaigns that not only catch eyes but also touch hearts and minds.",
     thumbnail:
       "https://res.cloudinary.com/dhbfad5qc/image/upload/v1708504654/common/leaves-6975462_1280_j3a6uf.webp",
-    page: "./insights/craftingcompellingcampaignsdrivenbydataledcommunication.html",
+    page: "./insights/compellingcampaignsdrivenbydataledcommunication.html",
   },
   {
-    title: "AI Revolution: Transforming Marketing Strategies.",
+    title: "Transforming Marketing Strategies with AI",
     description:
       "The fast progress of artificial intelligence (AI) has completely changed how businesses work. It's affecting nearly every industry, including marketing. AI helps marketers access loads of data and analytics they couldn't get before. This means they can create better-targeted marketing plans, which are reshaping how businesses connect with their audience.",
     thumbnail:
       "https://res.cloudinary.com/dhbfad5qc/image/upload/v1708504654/common/leaves-6975462_1280_j3a6uf.webp",
-    page: "./insights/airevolutiontransformingmarketingstrategies.html",
+    page: "./insights/transformingmarketingstrategieswithAI.html",
   },
   {
-    title: "Video Domination - Trends and Strategies For The New Era.",
+    title: "Unlocking Growth in the Age of Video Domination",
     description:
       "In today's digital landscape, video has become a dominant force, reshaping the way people interact with brands and access information. At Atomyc, we're deeply engaged in leveraging the potential of video content, recognizing its transformative impact.",
     thumbnail:
       "https://res.cloudinary.com/dhbfad5qc/image/upload/v1708504654/common/leaves-6975462_1280_j3a6uf.webp",
-    page: "./insights/videodominationtrendsandstrategiesforthenewera.html",
+    page: "./insights/unlockinggrowthintheageofvideodomination.html",
   },
 ];
